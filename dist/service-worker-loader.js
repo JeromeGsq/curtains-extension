18062358234ee1377d8599378440d6c233199f8c
JeromeGsq <jerome.ghesquiere@gmail.com>
2025-10-15 15:10:29 +0200

    release: 2025_10_15_15_10_28

--- dist/service-worker-loader.js
+++ dist/service-worker-loader.js
@@ -1,3 +1,1 @@
-import 'http://localhost:5173/@vite/env';
-import 'http://localhost:5173/@crx/client-worker';
-import 'http://localhost:5173/src/service_worker.ts';
+import './assets/service_worker.ts-CIbhEhnl.js';
